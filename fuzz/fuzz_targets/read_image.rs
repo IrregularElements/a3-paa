6ea0893d6da426bf73a0deefce6a84f91e95e102
inty <interruptinuse@gmail.com>
2022-01-24 17:51:30 +0000

    fuzz: In-tree init

---
+++ fuzz/fuzz_targets/read_image.rs
@@ -0,0 +1,15 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+use std::io::Cursor;
+
+use a3_paa::PaaImage;
+
+fuzz_target!(|data: &[u8]| {
+	let mut cursor = Cursor::new(data);
+	let image = PaaImage::read_from(&mut cursor);
+
+	if let Ok(image) = image {
+		let _ = image.to_bytes();
+	};
+});
