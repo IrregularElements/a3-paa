b6876c86d32edc79756e53a21e24617d32642dec
inty <interruptinuse@gmail.com>
2022-02-08 12:54:40 +0000

    Use bc::LzssReader for LZSS decompression

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,7 @@ use bohemia_compression::{
 	Algorithm,
 	RleReader,
 	RleWriter,
+	LzssReader,
 };
 use static_assertions::const_assert;
 use bstr::BString;
@@ -114,7 +115,7 @@ pub enum PaaError {
 	LzssCompressError(#[error(ignore)] String),
 
 	/// LZSS decompression failed.
-	LzssDecompressError(#[error(ignore)] String),
+	LzssDecompressError,
 
 	/// [`PaaMipmap::read_from`] was passed an LZSS-compressed [`PaaMipmap`]
 	/// with incorrect additive checksum, or LZSS decompression resulted in
@@ -1180,67 +1181,14 @@ pub fn decompress_lzo_slice(input: &[u8], dst_len: usize) -> PaaResult<Vec<u8>> 
 }
 
 
-#[allow(unused_variables)]
 pub fn decompress_lzss_slice(input: &[u8], dst_len: usize) -> PaaResult<Vec<u8>> {
-	// See https://opensource.apple.com/source/xnu/xnu-201/libsa/mkext.c.auto.html
-	const N: usize = 4096;
-	const F: usize = 18;
-	const THRESHOLD: usize = 2;
+	let data = LzssReader::new().filter_slice_to_vec(input).unwrap();
 
-	let mut dst_buf: Vec<u8> = vec![0; dst_len];
-	let mut dst: usize = 0;
-	let mut src: usize = 0;
-	let srcend = input.len();
+	if data.len() != dst_len {
+		return Err(LzssDecompressError);
+	};
 
-	let mut text_buf = [0u8; N + F - 1];
-
-	let mut i: libc::c_int;
-	let mut j: libc::c_int;
-	let mut k: libc::c_int;
-	let mut r: libc::c_int = (N - F) as libc::c_int;
-	let mut c: libc::c_int;
-	let mut flags: libc::c_uint = 0;
-
-	#[allow(clippy::needless_range_loop)]
-	for i in 0..N-F {
-		text_buf[i] = 0x20;
-	}
-
-	loop {
-		flags >>= 1;
-
-		if (flags & 0x100) == 0 {
-			if src < srcend { c = input[src].into(); src += 1; } else { break; };
-
-			flags = libc::c_uint::from_le_bytes(c.to_le_bytes()) | 0xFF00;
-		}
-
-		if flags & 1 != 0 {
-			if src < srcend { c = input[src].into(); src += 1; } else { break; };
-			dst_buf[dst] = (c & 0xFF) as u8;
-			dst += 1;
-			text_buf[r as usize] = (c & 0xFF) as u8;
-			r += 1;
-			r &= (N - 1) as libc::c_int;
-		}
-		else {
-			if src < srcend { i = input[src].into(); src += 1; } else { break; };
-			if src < srcend { j = input[src].into(); src += 1; } else { break; };
-			i |= (j & 0xF0) << 4;
-			j = (j & 0x0F) + THRESHOLD as libc::c_int;
-
-			for k in 0..=j {
-				c = text_buf[(i + k) as usize & (N - 1)] as libc::c_int;
-				dst_buf[dst] = c as u8;
-				dst += 1;
-				text_buf[r as usize] = c as u8;
-				r += 1;
-				r &= (N - 1) as libc::c_int;
-			}
-		}
-	}
-
-	Ok(dst_buf)
+	Ok(data)
 }
 
 
