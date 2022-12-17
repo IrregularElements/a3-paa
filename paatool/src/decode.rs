aee2a8ab1450c8bd49ab73baa183fe89c2a0f72a
inty <interruptinuse@gmail.com>
2022-12-17 13:14:02 +0000

    Fix 1-based mipmap index underflow

--- paatool/src/decode.rs
+++ paatool/src/decode.rs
@@ -6,7 +6,9 @@ pub fn command_decode(matches: &clap::ArgMatches) -> AnyhowResult<()> {
 	let paa_path = matches.value_of("paa").expect("PAA required");
 	let png_path = matches.value_of("png").expect("PNG required");
 	let mip_idx_str = matches.value_of("mipmap").unwrap_or("1");
-	let mip_idx = mip_idx_str.parse::<usize>().with_context(|| format!("Failed to parse mipmap index from -m{mip_idx_str}"))?;
+	let mip_idx = mip_idx_str.parse::<usize>()
+		.with_context(|| format!("Could not parse mipmap index from \"{mip_idx_str}\""))
+		.and_then(|i| if i > 0 { Ok(i) } else { Err(anyhow::anyhow!("Mipmap index cannot be 0")) })?;
 
 	let mut paa_file = std::fs::File::open(paa_path).with_context(|| format!("Could not open file: {paa_path}"))?;
 	let image = PaaImage::read_from(&mut paa_file).with_context(|| format!("Could not read PaaImage: {paa_path}"))?;
